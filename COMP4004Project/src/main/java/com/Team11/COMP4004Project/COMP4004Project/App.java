7490e592e9fe9d5114a786fae9896456599cfc35
NamCao <namcaonguyen@cmail.carleton.ca>
2020-10-19 11:37:32 -0400

    Initial Commit for the Project

---
+++ COMP4004Project/src/main/java/com/Team11/COMP4004Project/COMP4004Project/App.java
@@ -0,0 +1,13 @@
+package com.Team11.COMP4004Project.COMP4004Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
