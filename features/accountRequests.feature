e510326d8352be5716faadf7e6d669d044e1321c
Zahid Dawod <zahiddawod@hotmail.com>
2020-11-09 18:35:17 -0500

    Zahid - TEST&CODE - Feature where admins can approve and decline accounts that are pending registration

---
+++ features/accountRequests.feature
@@ -0,0 +1,12 @@
+Feature: Manage Account Requests
+
+	Scenario: An admin approves an account request
+		Given A user registers with the account info "student", "Bob Vance", "bob_vance@cms.com", "password"
+		When The admin approves them
+		Then The user is able to login
+        
+	Scenario: An admin declines an account request
+		Given A user registers with the account info "administrator", "Mike Hunt", "mike_hunt@cms.com", "password"
+		When The admin declines them
+		Then The user does not exist in the database anymore
+        And The user is not able to login
---
+++ features/steps/accountApprovalStepDefs.js
@@ -0,0 +1,71 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+const User = require("../../db/user.js");
+
+Given("A user registers with the account info {string}, {string}, {string}, {string}", function(accountTypeParam, fullnameParam, emailParam, passwordParam) {
+    this.user = new User({
+        email: emailParam,
+        password: passwordParam,
+        fullname: fullnameParam,
+        accountType: accountTypeParam,
+        approved: false
+    });
+
+    this.user.save(function (err) {
+        if ( err ) {
+            assert(false);
+            throw err;
+        }
+        assert(true);
+    });
+});
+
+When("The admin approves them", async function() {
+    await User.update({ _id: this.user._id }, { $set: { approved: true }}, function(err) {
+        if (err) {
+            assert(false);
+            throw err;
+        }
+        assert(true);
+    });
+});
+
+When("The admin declines them", async function() {
+    await User.deleteOne({ _id: this.user._id }, function(err) {
+        if (err) {
+            assert(false);
+            throw err;
+        }
+        assert(true);
+    });
+});
+
+Then("The user does not exist in the database anymore", async function() {
+    await User.find({ email: this.user.email, password: this.user.password }, function(err, result) {
+        if (err) {
+            assert(false);
+            throw err;
+        }
+        assert.strictEqual(0, result.length);
+    });
+});
+
+Then("The user is able to login", async function() {
+    await User.find({ email: this.user.email, password: this.user.password }, function(err, result) {
+        if ( err ) {
+            assert(false);
+            throw err;
+        }
+        assert.strictEqual(true, (result.length > 0 && result[0].approved));
+    });
+});
+
+Then("The user is not able to login", async function() {
+    await User.find({ email: this.user.email, password: this.user.password }, function(err, result) {
+        if ( err ) {
+            assert(false);
+            throw err;
+        }
+        assert.strictEqual(true, (result.length === 0 || !result[0].approved));
+    });
+});
