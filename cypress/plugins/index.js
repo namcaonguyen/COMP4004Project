4fcff651f4a9c5efb52ab2f1eede6737709a92f2
NamCao <namcaonguyen@cmail.carleton.ca>
2020-12-07 16:39:40 -0500

    NamCao - TEST - Cypress tests for account registration and login after approval
    This commit adds Cypress test cases for a student/professor registering for an account, and loggin in after being approved by an administrator.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,4 +14,79 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  on('task', {
+    // Task to log a message.
+    log(message) {
+      console.log(message)
+
+      return null
+	}
+  })
+
+  on('task', {
+    // Task to clear the database for testing.
+    clearDatabase() {
+      const User = require("../../db/user.js");
+      const Course = require("../../db/course.js");
+      const Class = require("../../db/class.js");
+      const ClassEnrollment = require("../../db/classEnrollment.js");
+      const Deliverable = require("../../db/deliverable.js");
+      const DeliverableSubmission = require("../../db/deliverableSubmission.js");
+
+      // Set up the MongoDB.
+      const mongoose = require("mongoose");
+      mongoose.connect("mongodb://localhost/cmsApp");
+      const db = mongoose.connection;
+      db.on("error", console.error.bind(console, "MongoDB Connection Error:"));
+
+      // Clear the database.
+      // NOTE: DO NOT USE THE 'AWAIT' KEYWORD HERE!!! IT DOES NOT WORK!!!
+
+      // Delete all the student Users.
+      User.deleteMany({accountType: "student"}, function(err) {
+        if ( err ) {
+          console.log(err);  
+		}
+      });
+      // Delete all the professor Users.
+      User.deleteMany({accountType: "professor"}, function(err) {
+        if ( err ) {
+          console.log(err);  
+		}
+      });
+      // Delete all the Courses.
+      Course.deleteMany({}, function(err) {
+        if ( err ) {
+          console.log(err);  
+		}
+	  });
+      // Delete all the Classes.
+      Class.deleteMany({}, function(err) {
+        if ( err ) {
+          console.log(err);  
+		}
+	  });
+      // Delete all the ClassEnrollments.
+      ClassEnrollment.deleteMany({}, function(err) {
+        if ( err ) {
+          console.log(err);  
+		}
+	  });
+      // Delete all the Deliverables.
+      Deliverable.deleteMany({}, function(err) {
+        if ( err ) {
+          console.log(err);  
+		}
+	  });
+      // Delete all the DeliverableSubmissions.
+      DeliverableSubmission.deleteMany({}, function(err) {
+        if ( err ) {
+          console.log(err);  
+		}
+	  });
+      
+      return null
+	}
+  })
 }
